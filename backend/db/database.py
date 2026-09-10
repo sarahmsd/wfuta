6051e398088820f180f0ec556c4df69eadd1a940
sarahmsd <56982584+sarahmsd@users.noreply.github.com>
2026-09-10 18:49:16 +0000

    fix: configure database with environment variable

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,7 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://sd:password@localhost:5432/wfuta"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(DATABASE_URL)
 
 def get_db():
